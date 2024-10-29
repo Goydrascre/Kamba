245a85728cb2e7b9b27c3e946576ea6918620f1d
John Doe <andreasbolstad@outlook.com>
2024-10-29 10:51:15 +0100

    Co-authored-by: P-stoltz <P-stoltz@users.noreply.github.com>dwd

--- javascripts/java.js
+++ javascripts/java.js
@@ -1,4 +1,6 @@
-function addToCart() {
-    alert("Item added to cart!") 
-}
+let count = 0;
 
+document.getElementById("counterButton").addEventListener("click", function() {
+    count++;
+    document.getElementById("countDisplay").innerText = count;
+});
