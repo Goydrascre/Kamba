39359333375978e98c767ebc1291bf3d5b15678d
John Doe <andreasbolstad@outlook.com>
2024-11-19 10:30:45 +0100

    Co-authored-by: P-stoltz <P-stoltz@users.noreply.github.com>

--- javascripts/java.js
+++ javascripts/java.js
@@ -4,3 +4,14 @@ document.getElementById("counterButton").addEventListener("click", function() {
     count++;
     document.getElementById("countDisplay").innerText = count;
 });
+function openFullscreen(imageElement) {
+    const fullscreenContainer = document.getElementById("fullscreenContainer");
+    const fullscreenImage = document.getElementById("fullscreenImage");
+    fullscreenImage.src = imageElement.src; // Setter kildebildet til fullskjermvisningen
+    fullscreenContainer.style.display = "flex";
+}
+
+function closeFullscreen() {
+    const fullscreenContainer = document.getElementById("fullscreenContainer");
+    fullscreenContainer.style.display = "none";
+}
